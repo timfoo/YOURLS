b9bd778e98c30ddccc6925624328d336560d8009
timfoo <30102954+timfoo@users.noreply.github.com>
2020-08-07 22:57:46 +0800

    Update config.php

--- user/config.php
+++ user/config.php
@@ -67,7 +67,7 @@ $yourls_user_passwords = array(
 
 /** Debug mode to output some internal information
  ** Default is false for live site. Enable when coding or before submitting a new issue */
-define( 'YOURLS_DEBUG', false );
+define( 'YOURLS_DEBUG', true );
 
 /*
  ** URL Shortening settings
